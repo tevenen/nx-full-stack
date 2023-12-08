f7ce7d66d0817a3b728e931ce08a876c5b161dbb
tevenen <milan.jovkovic93@gmail.com>
2023-12-08 21:40:30 +0100

    Added angular material

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, MatCardModule],
   selector: 'my-workspace-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
